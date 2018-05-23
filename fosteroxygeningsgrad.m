5154911ea7b8bc39dd99e59c427a3a2dfcbfa737
MelanieJensen93 <melanie-jensen93@hotmail.com>
2018-05-23 15:32:39 +0200

    Callback SliderLayer

--- fosteroxygeningsgrad.m
+++ fosteroxygeningsgrad.m
@@ -83,6 +83,7 @@ function SliderROIPicture_Callback(hObject, eventdata, handles)
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
 
 
+
 % --- Executes during object creation, after setting all properties.
 function SliderROIPicture_CreateFcn(hObject, eventdata, handles)
 % hObject    handle to SliderROIPicture (see GCBO)
@@ -105,6 +106,21 @@ function SliderLayer_Callback(hObject, eventdata, handles)
 %        get(hObject,'Min') and get(hObject,'Max') to determine range of slider
 % Position af slideren = ergo det billede vi vil have
 
+%Henter slider værdien og runder værdien op til nærmeste heltal. 
+ImPos = ceil(get(handles.SliderLayer, 'Value'));
+%Sætter teksten under slider, der fortæller hvilket snit der bliver vist ud
+%af x-antal i alt. 
+set(handles.txtSliderLayer, 'String', sprintf('%d/%d',ImPos,length([handles.MyData.Layers])));
+
+%Fortæller hvor snit-billederne skal vises. 
+axes(handles.axLayers)
+clear axes;
+%Der er altid 4 billeder pr. række i montage. 
+antalRaekker = handles.MyData.Layers(ImPos)/4;
+%Montage vise alle snitbillederne i et stort billede. 
+%Strack(ImPos).Strack - fortæller hvilken strack der skal bruges ud fra
+%snitposisionen, og så finder den alle billedern i det snit. 
+montage([handles.MyData.Stacks(ImPos).Stack], 'Size', [antalRaekker,4])
 
 % --- Executes during object creation, after setting all properties.
 function SliderLayer_CreateFcn(hObject, eventdata, handles)
@@ -204,11 +220,11 @@ set(handles.SliderLayer, 'Max', handles.MyData.NumbOfLayers);
 set(handles.SliderLayer, 'SliderStep', [1/((handles.MyData.NumbOfLayers)-1), 2/((handles.MyData.NumbOfLayers)-1)]);
 guidata(hObject, handles);
 
-% %Initialiserer sliderROIPicture
-% set(handles.SliderROIPicture, 'Value', 1);
-% set(handles.SliderROIPicture, 'Min', 1);
-% set(handles.SliderROIPicture, 'Max', size(handles.MyData.T2.LayerNo)); 
-% set(handles.SliderROIPicture, 'SliderStep', [1/(handles.MyData.LayerNo-1), 2/(handles.MyData.LayerNo-1)]);
+ %Initialiserer sliderROIPicture
+ set(handles.SliderROIPicture, 'Value', 1);
+ set(handles.SliderROIPicture, 'Min', 1);
+ set(handles.SliderROIPicture, 'Max', size(handles.MyData.Layers)); 
+ set(handles.SliderROIPicture, 'SliderStep', [1/(handles.MyData.LayerNo-1), 2/(handles.MyData.LayerNo-1)]);
 
 
 % Position af slideren = ergo det billede vi vil have
