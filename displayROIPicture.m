8dacc7f7b6870f159d34ab084c8b3ae9a063338a
MelanieJensen93 <melanie-jensen93@hotmail.com>
2018-05-24 17:12:33 +0200

    display

--- displayROIPicture.m
+++ displayROIPicture.m
@@ -1,6 +1,6 @@
 function handles = displayROIPicture(handles)
 %DisplayROIPicture Viser billederne i det valgte snit i axes
-%   
+
 ImPos = ceil(get(handles.SliderLayer, 'Value'));
  ImPosROI = round(get(handles.SliderROIPicture, 'Value'));
  set(handles.txtSliderROIPicture, 'String', sprintf('%d/%d', ImPosROI, length(handles.MyData.Layers(ImPos).Images))); 
