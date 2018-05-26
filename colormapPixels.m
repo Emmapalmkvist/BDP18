83e97b0eced6a0ff7d6383aae03991eae876df35
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-05-26 17:07:17 +0200

    Colormap

--- colormapPixels.m
+++ colormapPixels.m
@@ -13,6 +13,8 @@ for i = 1:length(idx)
     pic(idx(i)) = handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(i).rsquare;
 end
 
+% Normaliser T2-værdierne til at ligge mellem 0-1
+imNorm = (im-min(im(:)))/(max(im(:))-min(im(:)));
 
 % Find det nuværende billede
 ImPos = ceil(get(handles.SliderLayer, 'Value'));
@@ -36,8 +38,9 @@ h = subimage(currentIm);
 
 % Sæt aksen til at vise det, som ligger inden i mask
 set(h, 'AlphaData', mask);
-% Vis indholdet inden i ROIen og farvelæg det med jet-colormappen
-imagesc(pic); colormap(jet)
+% Vis indholdet inden i ROIen og farvelæg det med jet-colormappen (som er
+% flippet: rød = 1, blå = 0)
+imagesc(imNorm); colormap(flipud(jet))
 hold on;
 % Læg billedet på igen og få et handle dertil
 h = subimage(currentIm);
