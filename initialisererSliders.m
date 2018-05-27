436b467164471b015d9ba390d250f564d306006f
MelanieJensen93 <melanie-jensen93@hotmail.com>
2018-05-27 17:22:57 +0200

    Oprydning

--- initialisererSliders.m
+++ initialisererSliders.m
@@ -7,20 +7,16 @@ function handles = initialisererSliders(handles)
 %   billeder i et snit i ROIPicture.
 %   - SliderStep, fortæller hvor langt slideren skal flyttes af gangen. 
 
-
-
 %Initialiserer sliderROIPicture
  set(handles.SliderROIPicture, 'Value', 1);
  set(handles.SliderROIPicture, 'Min', 1); 
  set(handles.SliderROIPicture, 'Max', length(handles.MyData.Layers(1).Images)); 
  set(handles.SliderROIPicture, 'SliderStep', [1/length(handles.MyData.Layers(1).Images), 2/length(handles.MyData.Layers(1).Images)]);
 
- %Initialiserer sliderLayer 
-set(handles.SliderLayer, 'Value', 1);
+%Initialiserer sliderLayer 
+set(handles.Slid,erLayer, 'Value', 1);
 set(handles.SliderLayer, 'Min', 1);
 set(handles.SliderLayer, 'Max', handles.MyData.NumbOfLayers);
 set(handles.SliderLayer, 'SliderStep', [1/((handles.MyData.NumbOfLayers)-1), 2/((handles.MyData.NumbOfLayers)-1)]);
-
-%guidata(hObject, handles);
 end
 
