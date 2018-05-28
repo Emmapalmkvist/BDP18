1b620f8982383bf59be2aaf33bfe4105fd085b7f
MelanieJensen93 <melanie-jensen93@hotmail.com>
2018-05-28 19:30:18 +0200

    halløj ting

--- displayROIPicture.m
+++ displayROIPicture.m
@@ -2,11 +2,12 @@ function handles = displayROIPicture(handles)
 %DISPLAYROIPICTURE viser hvert enkelt billede i det valgte snit.
 %Hivlket billede der er vist skifter, når brugeren flytter på slideren.
     
-cla(handles.axDrawROI)
+ cla(handles.axDrawROI)
 
  layerPos = ceil(get(handles.SliderLayer, 'Value'));
  echoPos = round(get(handles.SliderROIPicture, 'Value'));
- set(handles.txtSliderROIPicture, 'String', sprintf('%d/%d', echoPos, length(handles.MyData.Layers(layerPos).Images))); 
+ set(handles.txtSliderROIPicture, 'String', sprintf('%d/%d', echoPos,...
+     length(handles.MyData.Layers(layerPos).Images))); 
  axes(handles.axDrawROI)
  %Normalisering
  currentIm = double(handles.MyData.Layers(layerPos).Images(echoPos).Image);
