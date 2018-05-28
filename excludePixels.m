b04d48d59636e767e8cee1a25c84c4a00e606bce
Anni Lykke Wilhelmsen <201509504@post.au.dk>
2018-05-28 17:08:00 +0200

    Forklarende tekst i top, kommentarer i koden

--- excludePixels.m
+++ excludePixels.m
@@ -1,25 +1,42 @@
 function handles = excludePixels(handles, ROIID, type, boundary)
-%EXCLUDEPIXELS Eksluderer dårligt fittede pixels ud fra de specificerede
-%værdier
+%EXCLUDEPIXELS Eksluderer dårligt fittede pixels ud fra den specificerede
+%værdi
+%   Frasorterer de pixels, som har et Goodness of Fit, som ligger uden for
+%   den specificerede værdi (boundary). Derefter farvelægges der på ny og
+%   en ny, revideret T2*-værdi beregnes.
+%
+%   INPUT:
+%   handles: handle til elementer i gui
+%   ROIID: et id (placering i handles) på den ROI, der ekskluderes pixels
+%   fra
+%   type: hvilken Goodness of Fit-type der er valgt at ekskludere ud fra
+%   (RMSE eller R^2)
+%   boundary: den specificerede værdi, som der skal ekskluderes ud fra
+%
+%   OUTPUT:
+%   handles med nye værdier returneret fra funktionerne: 
+%       - colormapPixels
+%       - getMeanRoi
+%       - fitMeanIntensities
 
 layerPos = get(handles.SliderLayer, 'Value');
 
 % Tjek hvilken type værdi der er valgt at sortere efter: RMSE eller R^2
 if(strcmp(type, 'rmse'))
-    % Find placering af de værdier, som er større end eller lig den specificerede værdi
-    valueLoc = [handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rmse] >= boundary;
+    % Find placering af de værdier, som er større end eller lig den
+    % specificerede værdi
+    valueLoc = ...
+     [handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rmse]...
+     >= boundary;
 elseif(strcmp(type, 'R^2'))
-    % Find placering af de værdier, som er større end eller lig den specificerede værdi
-    valueLoc = [handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rsquare] <= boundary;
+    % Find placering af de værdier, som er større end eller lig den
+    % specificerede værdi
+    valueLoc = ...
+     [handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rsquare]...
+     <= boundary;
 end
 
-% Find placering af de værdier, som er større end eller lig den specificerede
-% værdi
-%rmseLoc = find([handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rmse] >= boundary);
-%valueLoc = [handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).GOF(:).rmse] >= boundary;
-
-
-% Find deres tilhørende index i billede
+% Find værdiernes tilhørende index i billedet
 idx = handles.MyData.Layers(layerPos).ROIS(ROIID).ROI.EchoPix(1).Indexes;
 % Fjern disse indexes
 idx(valueLoc) = [];
@@ -28,7 +45,8 @@ idx(valueLoc) = [];
 handles = colormapPixels(handles, ROIID, layerPos, idx);
 
 % Lav en maske
-mask = zeros(256,256);
+sz = size(handles.MyData.Layers(layerPos).Images(1).Image);
+mask = zeros(sz);
 mask(idx) = 1;
 
 % Få gennemsnitsværdier for tilbageværende pixels
